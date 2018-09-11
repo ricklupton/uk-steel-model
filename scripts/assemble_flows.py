0f1541a4dd4dfca93b6a12eaeee7119c898bdcde
Rick Lupton <mail@ricklupton.name>
2018-09-11 09:19:27 +0100

    Update assemble_flows script to deal with multiple years

--- scripts/assemble_flows.py
+++ scripts/assemble_flows.py
@@ -20,7 +20,7 @@ logger.setLevel(logging.DEBUG)
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 
 # Load data tables
-ints = pd.read_csv('data/intermediate_products.csv', index_col='product')
+intermediate = pd.read_csv('data/intermediate_products.csv', index_col=['year', 'product'])
 
 alloc = load_dataframe('allocations/datapackage.json', 'product_sector_allocations') \
     .set_index('product') \
@@ -35,12 +35,10 @@ assert all(sector_yield_losses >= 0), 'sector yield losses should be greater tha
 
 product_imports = load_dataframe(
     os.path.join(ROOT, 'uk-steel-trade/datapackage.json'), 'imports') \
-    .query('year == 2016') \
-    .set_index('product_group')
+    .set_index(['year', 'product_group'])
 product_exports = load_dataframe(
     os.path.join(ROOT, 'uk-steel-trade/datapackage.json'), 'exports') \
-    .query('year == 2016') \
-    .set_index('product_group')
+    .set_index(['year', 'product_group'])
 
 ###########################################################################
 # 2. Build list of flows
@@ -57,33 +55,48 @@ product_lookup = {
     'Other sectors': 'Other industries',
 }
 
-flows = []
-sector_output = defaultdict(float)
+def build_flows_for_year(year):
+    flows = []
+    sector_output = defaultdict(float)
 
-for p in ints.index.values:
-    # Up to UK intermediate products: flows A and B
-    flows.append(('uk_production', 'uk_intermediate', p,
-                  ints.loc[p, 'production'] - ints.loc[p, 'exports']))
-    flows.append(('uk_production', 'exports', p, ints.loc[p, 'exports']))
-    flows.append(('imports', 'uk_intermediate', p, ints.loc[p, 'imports']))
+    # Data for this year
+    ints = intermediate.loc[year]
 
-    # From UK intermediate products to sectors that use them: flow C, D, E
-    delivered = ints.loc[p, 'delivered']
-    for s in alloc.columns:
-        x = delivered * alloc.loc[p, s]
-        y = sector_yield_losses.loc[p, s]
-        prod = product_lookup[s]
-        flows.append(('uk_intermediate', s, p, x))  # flow C
-        flows.append((s, 'scrap', 'scrap ' + p, x * y))  # flow D
-        flows.append((s, 'products', prod, x * (1 - y)))  # flow E
-        sector_output[prod] += x * (1 - y)
+    for p in ints.index.values:
+        # Up to UK intermediate products: flows A and B
+        flows.append(('uk_production', 'uk_intermediate', p,
+                    ints.loc[p, 'production'] - ints.loc[p, 'exports']))
+        flows.append(('uk_production', 'exports', p, ints.loc[p, 'exports']))
+        flows.append(('imports', 'uk_intermediate', p, ints.loc[p, 'imports']))
 
-# Finished products imports and exports
-for prod in product_imports.index:
-    exports = float(product_exports.loc[prod, 'mass_iron'])
-    flows.append(('products', 'product_exports', prod, exports))
-    flows.append(('products', 'uk_demand', prod, sector_output[prod] - exports))
-    flows.append(('product_imports', 'uk_demand', prod, product_imports.loc[prod, 'mass_iron']))
+        # From UK intermediate products to sectors that use them: flow C, D, E
+        delivered = ints.loc[p, 'delivered']
+        for s in alloc.columns:
+            x = delivered * alloc.loc[p, s]
+            y = sector_yield_losses.loc[p, s]
+            prod = product_lookup[s]
+            flows.append(('uk_intermediate', s, p, x))  # flow C
+            flows.append((s, 'scrap', 'scrap ' + p, x * y))  # flow D
+            flows.append((s, 'products', prod, x * (1 - y)))  # flow E
+            sector_output[prod] += x * (1 - y)
 
-flows = pd.DataFrame(flows, columns=('source', 'target', 'material', 'value'))
-flows.to_csv('data/flows.csv', index=False)
+    # Finished products imports and exports
+    for prod in product_imports.index.levels[1]:
+        exports = float(product_exports.loc[(year, prod), 'mass_iron'])
+        imports = float(product_imports.loc[(year, prod), 'mass_iron'])
+        flows.append(('products', 'product_exports', prod, exports))
+        flows.append(('products', 'uk_demand', prod, sector_output[prod] - exports))
+        flows.append(('product_imports', 'uk_demand', prod, imports))
+
+    flows = pd.DataFrame(flows, columns=('source', 'target', 'material', 'value'))
+    flows['year'] = year
+    return flows
+
+years = [2016]
+all_flows = pd.concat([
+    build_flows_for_year(year) for year in years
+])
+
+COLUMNS = ['source', 'target', 'material', 'year', 'value']
+
+all_flows[COLUMNS].to_csv('data/flows.csv', index=False)
